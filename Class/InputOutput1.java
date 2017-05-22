42ccf3fb5cf6f93969a6a0aded2b36028f397ec8
Syafardilah <syafardilah@gmail.com>
2017-05-22 22:49:18 +0700

    first commit

---
+++ Class/InputOutput1.java
@@ -0,0 +1,7 @@
+public class InputOutput1 {
+	public static void main (String[] args) {
+		System.out.print("Belajar Java Kuy0");
+		System.out.println("Belajar Java Kuy1");
+		System.out.print("Belajar Java Kuy2");
+	}
+}
---
+++ Enkapsuliasi Rambut/src/enkapsuliasi/rambut/EnkapsuliasiRambut.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package enkapsuliasi.rambut;
+
+/**
+ *
+ * @author windows7
+ */
+public class EnkapsuliasiRambut {
+    
+    private String jenis= "Keriting";
+    private String warna;
+    
+    //getter
+    public String getJenis() {
+        return jenis;
+    }
+    
+    public String getWarna() {
+        return warna;
+    }
+    
+    //setter
+    public void setJenis(String jenis) {
+        this.jenis = jenis;
+    }
+    
+    public void setWarna(String warna) {
+        this.warna = warna;
+    }
+}
+    
+
